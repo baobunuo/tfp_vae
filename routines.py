9deb6e9f1c20affeab8a01d03525afe64761f71d
Lucas Lingle <lucasdaxlingle@gmail.com>
2019-06-15 12:17:19 -0700

    dev-1 : update interpolation gif to make entire grid of images

--- routines.py
+++ routines.py
@@ -106,40 +106,70 @@ def interpolate(dataset, sess, model, callbacks):
 
 
 def interpolation_gif(dataset, sess, model, callbacks):
+    # creates a gif showing latent-space interpolations betwen randomly selected starting images and ending images
+    # the images are arranged in a square shape, so that many examples can be visualized simultaneously.
+
+    def make_image_grid(imgs, grid_height, grid_width):
+        assert len(imgs) == grid_height * grid_width
+        rows = []
+        for i in range(0, grid_height):
+            row = []
+            for j in range(0, grid_width):
+                img = imgs[grid_height * i + j]
+                row.append(img)
+            row = np.concatenate(row, axis=1)
+            rows.append(row)
+        rows = np.concatenate(rows, axis=0)
+        return rows
 
     for batch in tfds.as_numpy(dataset):
         batch_size = batch.shape[0]
         xs = batch
         permutation = np.random.permutation(batch_size)
         permuted_xs = xs[permutation]
+
+        # we'll make an S x S shaped grid of images
+        S = int(np.floor(np.sqrt(batch_size)).astype(np.int32))
+        xs = xs[0:S]
+        permuted_xs = permuted_xs[0:S]
+
+        # starting codes
         z0 = model.get_code(sess, xs)
+
+        # ending codes
         zT = model.get_code(sess, permuted_xs)
+
+        # intermediate codes z_i
         n = 8
-        ts = [(t / float(n - 1)) for t in range(0, n)]
+        ts = [(i / float(n - 1)) for i in range(0, n)]
         zs = [(1. - t) * z0 + t * zT for t in ts]
-        Z = np.stack(zs, axis=0)  # [T, B, Z]
-        Z = np.transpose(Z, [1, 0, 2])
+        Z = np.stack(zs, axis=0)       # [T, B, Z]
+        Z = np.transpose(Z, [1, 0, 2]) # [B, T, Z]
 
         print('saving images...')
-        for b in range(0, batch_size):
-            img_frames = []
-            img_frames.append(xs[b])
-            for i in range(0, n):
-                z_i = Z[b][i]
-                z_i = np.expand_dims(z_i, 0)
-                x_i = model.generate_from_code(sess, z_i)
-                x_i = np.squeeze(x_i, 0)
-                img_frames.append(x_i)
-            img_frames.append(permuted_xs[b])
-            
-            if x_i.shape[-1] == 1:
-                transform = lambda x_i: np.concatenate([x_i, x_i, x_i], axis=-1)
-                img_frames = list(map(transform, img_frames))
-            elif x_i.shape[-1] == 3:
-                transform = lambda x_i: 0.5 * x_i + 0.5
-                img_frames = list(map(transform, img_frames))
+        # make list of frames for the gif
+        img_frames = []
+        # include the starting images
+        img = make_image_grid(xs, S, S)
+        img_frames.append(img)
 
-            fp = callbacks['save_gif'](img_frames)
-            print(fp)
+        for i in range(0, n):
+            z_i = Z[:, i, :] # [B, Z]
+            x_i = model.generate_from_code(sess, z_i)
+            img = make_image_grid(x_i, S, S)
+            img_frames.append(img)
+
+        img = make_image_grid(permuted_xs, S, S)
+        img_frames.append(img)
+
+        if xs.shape[-1] == 1:
+            transform = lambda x_i: np.concatenate([x_i, x_i, x_i], axis=-1)
+            img_frames = list(map(transform, img_frames))
+        elif xs.shape[-1] == 3:
+            transform = lambda x_i: 0.5 * x_i + 0.5
+            img_frames = list(map(transform, img_frames))
+
+        fp = callbacks['save_gif'](img_frames)
+        print(fp)
 
         break
